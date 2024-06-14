f0404c9d9ae5710d4fb73d3dfc93794d4c01f815
genusistimelord <lordsatin@hotmail.com>
2024-06-14 10:05:23 -0400

    Changed fxhash to ahash for faster hashing. Also updated Mio to 1.0.0

--- src/containers.rs
+++ src/containers.rs
@@ -5,12 +5,10 @@ pub use bases::*;
 pub use storage::*;
 
 //We redefine these here so it is easier to update the hash style later if we need too.
-pub type FxBuildHasher = std::hash::BuildHasherDefault<ritehash::FxHasher>;
-
-pub type IndexMap<K, V> = indexmap::IndexMap<K, V, FxBuildHasher>;
-pub type IndexSet<T> = indexmap::IndexSet<T, FxBuildHasher>;
-pub type HashSet<T> = std::collections::HashSet<T, FxBuildHasher>;
-pub type HashMap<K, V> = std::collections::HashMap<K, V, FxBuildHasher>;
+pub type IndexMap<K, V> = indexmap::IndexMap<K, V, ahash::RandomState>;
+pub type IndexSet<T> = indexmap::IndexSet<T, ahash::RandomState>;
+pub type HashSet<T> = std::collections::HashSet<T, ahash::RandomState>;
+pub type HashMap<K, V> = std::collections::HashMap<K, V, ahash::RandomState>;
 
 // Salt used for encrypting passwords within database.
 pub const SALT: &[u8] = b"ThisIsMySalt";
